397ab7635c9f53699b374bf1a43ada088c311fce
OlgaEva <olga.rosas@gmail.com>
2020-01-28 09:48:03 -0400

    edit run file

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,21 @@ require 'json'
 require 'tty-prompt'
 require 'colorize'
 ActiveRecord::Base.logger = nil
+
+class Adventure
+    puts "Welcome to Choose Your Own Adventure!".colorize(:red)
+
+    def initialize
+        @prompt = TTY::Prompt.new
+        @current_user = nil
+    end
+
+    def initial_interaction
+        @prompt.select("Are you a new or returning User?") do |menu|
+            menu.choice "New" -> do
+                puts ("Welcome, please choose a Username:")
+                new_user = get.chomp
+                
+    end
+
+end
