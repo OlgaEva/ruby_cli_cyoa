e56a14707151352eeb18c75ab7e257b933ca8152
OlgaEva <olga.rosas@gmail.com>
2020-01-31 14:28:35 -0400

    add tables

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+gem 'activerecord', "~>5.2.3"
 gem "sinatra-activerecord"
-gem "sqlite3"
-gem "pry"
 gem "require_all"
+gem "sqlite3"
+gem 'rake'
+gem "pry"
 gem "faker"
 gem "tty-prompt"
 gem "rest-client"
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
-require 'bundler'
-Bundler.require
+ENV["SINATRA_ENV"] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV["SINATRA_ENV"])
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+require_all 'app'
